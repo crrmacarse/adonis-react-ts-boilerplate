5b8d5e63615c9dc300d3cfc3deeaa4a61defeb66
crrmacarse <crrmacarse@gmail.com>
2020-02-09 14:49:48 +0800

    Added Initial backend testing and https support

--- server.js
+++ server.js
@@ -17,9 +17,25 @@
 |     Make sure to pass relative path from the project root.
 */
 
-const { Ignitor } = require('@adonisjs/ignitor')
+// https://adonisjs.com/recipes/4.1/using-https
+const { Ignitor } = require('@adonisjs/ignitor');
+const https = require('https');
+const pem = require('pem');
 
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
+pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  const options = {
+    key: keys.serviceKey,
+    cert: keys.certificate
+  };
+
+  new Ignitor(require('@adonisjs/fold'))
+    .appRoot(__dirname)
+    .fireHttpServer((handler) => {
+      return https.createServer(options, handler)
+    })
+    .catch(console.error);
+  });
